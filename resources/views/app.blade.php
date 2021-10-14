2fbb3d4b0c8b4a82a599ff9711eaaa671c2f7dca
GilbertoASJ <alvesgilberto84@gmail.com>
2021-10-14 17:57:14 -0300

    Feature; :tada: First commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+    
+	<title>Newsletter</title>
+</head>
+<body>
+	@inertia
+</body>
+</html>
